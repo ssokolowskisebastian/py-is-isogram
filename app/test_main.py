3f821484e420fae711c8e26b567a3bc6c6d57da9
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-08-05 12:58:46 +0200

    solution isogram

--- app/test_main.py
+++ app/test_main.py
@@ -1,3 +1,16 @@
 from app.main import is_isogram
 
-# write your code here
+
+def test_is_isogram_true():
+    assert (is_isogram("seba"))
+    assert (is_isogram("abcd"))
+    assert (is_isogram(""))
+    assert (is_isogram("qwertyuiop"))
+    assert (is_isogram("zxcvbnm"))
+
+
+def test_is_isogram_false():
+    assert (not is_isogram("oppo"))
+    assert (not is_isogram("dada"))
+    assert (not is_isogram("bb"))
+    assert (not is_isogram("abba"))
