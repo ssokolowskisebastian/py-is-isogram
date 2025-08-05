75f572d49811d2096bfd97168f80000bdcf1e8ef
Sebastian Sokolowski <wielewiak08@gmail.com>
2025-08-05 13:48:21 +0200

    solution isogram

--- app/test_main.py
+++ app/test_main.py
@@ -2,15 +2,23 @@ from app.main import is_isogram
 
 
 def test_is_isogram_true() -> None:
-    assert (is_isogram("seba"))
-    assert (is_isogram("abcd"))
-    assert (is_isogram(""))
-    assert (is_isogram("qwertyuiop"))
-    assert (is_isogram("zxcvbnm"))
+    assert is_isogram("seba") is True
+    assert is_isogram("abcd") is True
+    assert is_isogram("qwertyuiop") is True
+    assert is_isogram("zxcvbnm") is True
 
 
 def test_is_isogram_false() -> None:
-    assert (not is_isogram("oppo"))
-    assert (not is_isogram("dada"))
-    assert (not is_isogram("bb"))
-    assert (not is_isogram("abba"))
+    assert is_isogram("oppo") is False
+    assert is_isogram("look") is False
+    assert is_isogram("pineapple") is False
+
+
+def test_is_isogram_case_sensitive() -> None:
+    assert is_isogram("OpPo") is False
+    assert is_isogram("LOok") is False
+    assert is_isogram("pineapPle") is False
+
+
+def test_is_isogram_empty_string() -> None:
+    assert is_isogram("") is True
